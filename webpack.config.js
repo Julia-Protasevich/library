4681ddff653322cec9e65bf6d847aa572257ee2a
Julia-Protasevich <protasevich.julia@gmail.com>
2020-03-23 22:24:52 +0300

    replace express server with webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  watch: false, 
-  mode: 'development', 
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }, 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' }
-        ]
-      }
-    ]
-  }, 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ]
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
 
-};
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]
+  
+  };
