3eeea8af33a96f0af33530c97fd667c80bc4f36b
Julia-Protasevich <protasevich.julia@gmail.com>
2020-06-20 23:59:30 +0300

    Implement env-dependent webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,68 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+module.exports = (env = {}) => {
+
+  const {mode = 'development'} = env;
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
+    ];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+      })
+    ];
+
+    if(isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css'
+      }));
+    }
+
+    if(isDev) {
+      plugins.push(new webpack.SourceMapDevToolPlugin({
+        test: /\.js$/,
+        filename: '[name].js.map',
+      }));
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
     module: {
-      rules: [
-        {
+      rules: [{
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-
-        },
-        {
+          loader: "babel-loader"
+        }, {
           test: /\.css$/,
-          use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' }
-          ]
-        },
-        {
+          use: getStyleLoaders()
+        }, {
+          test : /\.(s[c|a]ss)$/,
+          use: [...getStyleLoaders(), 'sass-loader']
+        }, {
           test: /\.html$/,
-          use: [
-            {
-              loader: "html-loader"
-            }
-          ]
-        }
+          loader: 'html-loader'
+        },
       ]
     },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
-      })
-    ]
+    plugins: getPlugins()
   
   };
+
+};
+
+
